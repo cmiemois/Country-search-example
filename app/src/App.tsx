a9edc1bb8584344f432f34e6d976e168837b59bf
Christoffer Miemois <christoffer.miemois@gmail.com>
2023-11-02 09:42:10 +0000

    UseEffect and Button() added, not ironed out

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const BASE_URL: URL = new URL('https://restcountries.com/v3.1/');
+
+function Button() {
+  const [loading, isloaded] = useState(true);
+  const [data, datapoints] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL+'all').then(res => res.json());
+    
+    // Add the stuff, make the stuff do the thing
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <button onClick={getFullList}>Load from API</button>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>);
+  } else {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><p>Hello, I am placeholder</p></div>
   );
+}
+
+async function getFullList(): Promise<Object> {
+  const response = await fetch(BASE_URL + 'all');
+  return response;
 }
 
 export default App;
